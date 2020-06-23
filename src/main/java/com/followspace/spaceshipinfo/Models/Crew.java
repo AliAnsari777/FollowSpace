e83565ecfbea1324f1fca2a7da0c8c61a274c4e6
ALi Ansari <alidk2013@gmail.com>
2020-06-22 22:50:05 -0500

    update crew model class and add postgreSQL in Heroku

--- src/main/java/com/followspace/spaceshipinfo/Models/Crew.java
+++ src/main/java/com/followspace/spaceshipinfo/Models/Crew.java
@@ -1,6 +1,7 @@
 package com.followspace.spaceshipinfo.Models;
 
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -9,15 +10,26 @@ public class Crew {
     private String message;
     private int number;
     private List<Map<String, String>> people;
+    private String expedition;
+    private LocalDate date;
+    private String about;
+    private String stationMember;
+    private String profilePic;
 
     public Crew() {
     }
 
-    public Crew(String message, String image, int number, List<Map<String, String>> people) {
-        this.message = message;
+    public Crew(String image, String message, int number, List<Map<String, String>> people,
+                String expedition, LocalDate date, String about, String stationMember, String profilePic) {
         this.image = image;
+        this.message = message;
         this.number = number;
         this.people = people;
+        this.expedition = expedition;
+        this.date = date;
+        this.about = about;
+        this.stationMember = stationMember;
+        this.profilePic = profilePic;
     }
 
     public String getMessage() {
@@ -50,5 +62,45 @@ public class Crew {
 
     public void setPeople(List<Map<String, String>> people) {
         this.people = people;
+    }
+
+    public String getExpedition() {
+        return expedition;
+    }
+
+    public void setExpedition(String expedition) {
+        this.expedition = expedition;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getStationMember() {
+        return stationMember;
+    }
+
+    public void setStationMember(String stationMember) {
+        this.stationMember = stationMember;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
     }
 }
